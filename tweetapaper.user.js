137ace34b47d55750f4b98aefe9684a6388f4619
Jason Karns <jason@karns.name>
2010-12-29 14:00:34 -0500

    First stab at tweetapaper script

---
+++ tweetapaper.user.js
@@ -0,0 +1,16 @@
+﻿// ==UserScript==
+// @name           Tweetapaper
+// @namespace      http://jason.karns.name
+// @include        http://www.instapaper.com/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
+
+
+$(".tableViewCell").each(function(index, element){
+  var link = $(element).find("a.tableViewCellTitleLink");
+  var t = {
+    text: link.text(),
+    url: link.attr("href")
+  };
+  $(element).find(".secondaryControls").append('<span class="separator">•</span> <a class="actionLink" href="http://twitter.com/share?'+$.param(t)+'" target="_blank">Tweet</a>');
+});
