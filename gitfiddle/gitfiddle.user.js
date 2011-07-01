8cbe4a997ca754bf757ee2e5fd64c3c211bbfa04
Jason Karns <jason@karns.name>
2011-07-01 11:33:44 -0400

    First stab at a userscript to add a jsfiddle link to a github gist that looks like a fiddle

---
+++ gitfiddle/gitfiddle.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           gitFiddle
+// @namespace      http://jason.karns.name
+// @include        https://gist.github.com/*
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js
+// @description    When on a GitHub gist that looks like it contains jsFiddle content, add a quick link to easily load the fiddle.
+// @author         Jason Karns
+// @version        0.1
+// @date           2011-07-01
+// @change         initial release
+// ==/UserScript==
+
+jQuery(function($){
+  function gist_is_a_fiddle(){
+    return $("#file_fiddle\\.manifest, .file[id^='file_fiddle']", $("#files")).size() > 1;
+  }
+  function insert_fiddle_link(){
+    $("#repos .meta table tbody").append('<tr><td class="label">jsFiddle URL:</td><td><a class="gist-jsfiddle-link" href="'+fiddle_link()+'">'+fiddle_link()+'</td></tr>');
+  }
+  function fiddle_link(){
+    return "http://jsfiddle.net/gh/gist/jquery/1.6" + location.pathname;
+  }
+  
+  if(gist_is_a_fiddle()) insert_fiddle_link();
+});
