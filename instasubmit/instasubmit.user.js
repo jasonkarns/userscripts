9d2e03c424b95b1c90c3ecbf0c090f8f747cfd53
Jason Karns <jason@karns.name>
2012-06-16 19:09:57 -0400

    match any instapaper/edit URL

--- instasubmit/instasubmit.user.js
+++ instasubmit/instasubmit.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name           Instasubmit
 // @namespace      http://jason.karns.name
-// @match          http://*.instapaper.com/edit?url=http%3A%2F%2Ffeedproxy.google.com*
-// @match          https://*.instapaper.com/edit?url=http%3A%2F%2Ffeedproxy.google.com*
+// @match          http://*.instapaper.com/edit?url=*
+// @match          https://*.instapaper.com/edit?url=*
 // @description    When sending articles to Instapaper from Google Reader via the SendTo mechanism, automatically submit the Instapaper form
 // @author         Jason Karns
-// @version        2.0
+// @version        2.0.0-alpha
 // ==/UserScript==
 
 var form = document.querySelector("form[action$=edit]");
