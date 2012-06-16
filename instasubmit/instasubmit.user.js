57c2dee2ca66fabde486a09e01f4d577837740cb
Jason Karns <jason@karns.name>
2012-06-16 18:41:44 -0400

    remove dependency on jQuery. add Chrome support

--- instasubmit/instasubmit.user.js
+++ instasubmit/instasubmit.user.js
@@ -3,13 +3,10 @@
 // @namespace      http://jason.karns.name
 // @match          http://*.instapaper.com/edit?url=http%3A%2F%2Ffeedproxy.google.com*
 // @match          https://*.instapaper.com/edit?url=http%3A%2F%2Ffeedproxy.google.com*
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @description    When sending articles to Instapaper from Google Reader via the SendTo mechanism, automatically submit the Instapaper form
 // @author         Jason Karns
-// @version        1.0
+// @version        2.0
 // ==/UserScript==
 
-console.log($);
-jQuery(function($){
-	$("#content form").submit();
-})();
+var form = document.querySelector("form[action$=edit]");
+form.__proto__.submit.apply(form);
