d7be5091c6556b728c5f305cc0987c635e7d850b
Jason Karns <jason@karns.name>
2012-06-16 18:41:10 -0400

    remove extraneous metadata

--- instasubmit/instasubmit.user.js
+++ instasubmit/instasubmit.user.js
@@ -7,8 +7,6 @@
 // @description    When sending articles to Instapaper from Google Reader via the SendTo mechanism, automatically submit the Instapaper form
 // @author         Jason Karns
 // @version        1.0
-// @date           2010-09-07
-// @change         initial release
 // ==/UserScript==
 
 console.log($);
