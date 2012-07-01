6b0b50d1297818f6059a67d5bc5b88bf412b44e4
Jason Karns <jason@karns.name>
2012-07-01 12:21:31 -0400

    namespace

--- gitfiddle/gitfiddle.spec.js
+++ gitfiddle/gitfiddle.spec.js
@@ -6,14 +6,14 @@ beforeEach(function() {
   });
 });
 
-describe('giddler',function(){
+describe('GitFiddle',function(){
   it('returns Gist if location is gist', function(){
     var location = "https://gist.github.com/606699/";
-    expect(giddler(location)).toDeriveFrom(Gist);
+    expect(GitFiddle(location)).toDeriveFrom(GitFiddle.Gist);
   });
   it('returns Repo if location is repo', function(){
     var location = "https://github.com/jasonkarns/";
-    expect(giddler(location)).toDeriveFrom(Repo);
+    expect(GitFiddle(location)).toDeriveFrom(GitFiddle.Repo);
   });
 });
 
@@ -24,34 +24,34 @@ describe('Gist', function(){
     });
     it('returns true if fiddle.css is found', function(){
       //affix("#files .file#file_fiddle\\.css");
-      expect(new Gist().sounds_like_a_fiddle()).toBeTruthy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
     });
     it('returns true if fiddle.html is found', function(){
       //affix("#files .file#file_fiddle\\.html");
-      expect(new Gist().sounds_like_a_fiddle()).toBeTruthy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
     });
     it('returns true if fiddle.js is found', function(){
       //affix("#files .file#file_fiddle\\.js");
-      expect(new Gist().sounds_like_a_fiddle()).toBeTruthy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
     });
     it('returns true if fiddle.manifest is found', function(){
       //affix("#files .file#file_fiddle\\.manifest");
-      expect(new Gist().sounds_like_a_fiddle()).toBeTruthy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
     });
   });
   describe('#sounds_like_a_fiddle - false', function(){
     it('returns false if fiddle.(html|css|js|manifest) is not found', function(){
-      expect(new Gist().sounds_like_a_fiddle()).toBeFalsy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeFalsy();
     });
     it('returns false if fiddle.fake is not found', function(){
       //affix("#files .file#file_fiddle\\.fake");
-      expect(new Gist().sounds_like_a_fiddle()).toBeFalsy();
+      expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeFalsy();
     });
   });
-  describe('#insert_fiddle_link', function(){
-    it('inserts fiddle link', function(){
+  describe('#insert_link', function(){
+    it('inserts link', function(){
       var meta = affix('#repos .meta table tbody');
-      new Gist({ pathname : '/606699/' }).insert_fiddle_link();
+      new GitFiddle.Gist({ pathname : '/606699/' }).insert_link();
       expect($(meta)).toContain('tr td.label');
       expect($(meta)).toContain('tr td a.gist-fiddle-link');
     });
@@ -63,33 +63,33 @@ describe('LinksGist', function(){
     it('is built from the gist id', function(){
       var location = { pathname : '/606699/' };
       var fiddle_url = 'http://jsfiddle.net/gh/gist/mootools/1.2/606699/';
-      expect(new LinksGist(location).url).toBe(fiddle_url);
+      expect(new GitFiddle.LinksGist(location).url).toBe(fiddle_url);
     });
     it('handles sha hash', function(){
       var location = { pathname : '/606699/35b6e9d1037cb33da0943ceccf5ddde6bd362263/' };
       var fiddle_url = 'http://jsfiddle.net/gh/gist/mootools/1.2/606699/';
-      expect(new LinksGist(location).url).toBe(fiddle_url);
+      expect(new GitFiddle.LinksGist(location).url).toBe(fiddle_url);
     });
   });
   describe('#link', function(){
     var location = { pathname : '/606699/35b6e9d1037cb33da0943ceccf5ddde6bd362263/' };
     var fiddle_url = 'http://jsfiddle.net/gh/gist/mootools/1.2/606699/';
     it('builds the link', function(){
-      var subject = new LinksGist(location).link;
+      var subject = new GitFiddle.LinksGist(location).link;
       //expect($(subject)).toBe('td a.gist-fiddle-link');
     });
     it('sets the href', function(){
-      var subject = new LinksGist(location).link;
+      var subject = new GitFiddle.LinksGist(location).link;
       //expect($(subject)).toHaveAttr('href', fiddle_url);
     });
     it('sets the text content', function(){
-      var subject = new LinksGist(location).link;
+      var subject = new GitFiddle.LinksGist(location).link;
       expect($(subject)).toHaveText(fiddle_url);
     });
   });
   describe('#label', function(){
     it('builds the label', function(){
-      //var subject = new LinksGist(location).label;
+      //var subject = new GitFiddle.LinksGist(location).label;
       //expect($(subject)).toBe('td.label');
     });
   });
--- gitfiddle/gitfiddle.user.js
+++ gitfiddle/gitfiddle.user.js
@@ -7,41 +7,42 @@
 // @version        0.1
 // ==/UserScript==
 
-var Gist = function(location){
-  this.sounds_like_a_fiddle = function(){
-    return document.querySelector("#files .file[id^=file_fiddle]");
+var GitFiddle = function(location){
+  GitFiddle.Gist = function(location){
+    this.location = location;
+    this.sounds_like_a_fiddle = function(){
+      return document.querySelector("#files .file[id^=file_fiddle]");
+    };
+    this.insert_link = function(){
+      var link = new GitFiddle.LinksGist(this.location);
+      var tr = document.createElement('tr');
+      tr.appendChild(link.label);
+      tr.appendChild(link.link);
+      document.querySelector('#repos .meta table tbody').appendChild(tr);
+    };
   };
-  this.insert_fiddle_link = function(){
-    var link = new LinksGist(location);
-    var tr = document.createElement('tr');
-    tr.appendChild(link.label);
-    tr.appendChild(link.link);
-    document.querySelector('#repos .meta table tbody').appendChild(tr);
+  GitFiddle.Repo = function(){};
+
+  GitFiddle.LinksGist = function(location){
+    var fiddle_base = 'http://jsfiddle.net/gh/gist/mootools/1.2/';
+    var gist_slug = location.pathname.match(/^\/([0-9]+)\//)[1] + '/';
+    var url = fiddle_base + gist_slug;
+
+    var link = document.createElement("td");
+    link.innerHTML = '<a class="gist-fiddle-link" href="'+ url +'">'+ url +'</a>';
+
+    var label = document.createElement("td");
+    label.className = 'label';
+
+    this.url = url;
+    this.link = link;
+    this.label = label;
   };
-};
-var Repo = function(){};
-
-var LinksGist = function(location){
-  var fiddle_base = 'http://jsfiddle.net/gh/gist/mootools/1.2/';
-  var gist_slug = location.pathname.match(/^\/([0-9]+)\//)[1] + '/';
-  var url = fiddle_base + gist_slug;
-
-  var link = document.createElement("td");
-  link.innerHTML = '<a class="gist-fiddle-link" href="'+ url +'">'+ url +'</a>';
-
-  var label = document.createElement("td");
-  label.className = 'label';
-
-  this.url = url;
-  this.link = link;
-  this.label = label;
-};
-
-function giddler(location){
-  if(location.match(new RegExp("^https://gist.github.com/"))){
-    return new Gist();
+  
+  switch(true){
+    case new RegExp("^https://gist.github.com/").test(location):
+      return new GitFiddle.Gist(location);
+    case new RegExp("^https://github.com/.+").test(location):
+      return new GitFiddle.Repo(location);
   }
-  if(location.match(new RegExp("^https://github.com/.+"))){
-    return new Repo();
-  }
-}
+};
