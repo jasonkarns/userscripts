2d6b3ef7951a06b1872019763a7c696cbbed222a
Jason Karns <jason@karns.name>
2012-07-01 23:04:04 -0400

    refactor GitFiddle to not require an initial call before its properties/sub-functions get defined

--- gitfiddle/gitfiddle.user.js
+++ gitfiddle/gitfiddle.user.js
@@ -8,59 +8,60 @@
 // ==/UserScript==
 
 var GitFiddle = function(location){
-  GitFiddle.Gist = function(location){
-    this.id = (location && location.pathname)? location.pathname.match(/^\/([0-9]+)\//)[1] : "";
-
-    this.sounds_like_a_fiddle = function(){
-      var selector = ["#files .file[id^=file_fiddle\\.css]",
-                      "#files .file[id^=file_fiddle\\.html]",
-                      "#files .file[id^=file_fiddle\\.js]",
-                      "#files .file[id^=file_fiddle\\.manifest]"].join(",");
-      return document.querySelector(selector);
-    };
-
-    this.insert_link = function(){
-      var fiddle_link = new GitFiddle.LinksGist(this.id).build();
-      document.querySelector('#repos .meta table tbody').appendChild(fiddle_link);
-    };
-  };
-  GitFiddle.Repo = function(){};
-
-  GitFiddle.LinksGist = function(gist){
-    this.url = (function(gist_id){
-      var fiddle_base = 'http://jsfiddle.net/gh/gist/mootools/1.2/';
-      return fiddle_base + gist_id + '/';
-    })(gist.id);
-
-    this.link = (function(url){
-      var link = document.createElement('a');
-      link.className = 'gist-fiddle-link';
-      link.href = url;
-      link.textContent = url;
-      return link;
-    })(this.url);
-
-    this.label = (function(){
-      var label = document.createElement("td");
-      label.textContent = 'Run Jasmine Specs';
-      label.className = 'label';
-      return label;
-    })();
-
-    this.build = function() {
-      var tr = document.createElement('tr');
-      tr.appendChild(this.label);
-      var td = document.createElement('td');
-      td.appendChild(this.link);
-      tr.appendChild(td);
-      return tr;
-    };
-  };
-
   switch(location.host){
     case "gist.github.com":
       return new GitFiddle.Gist(location);
     case "github.com":
       return new GitFiddle.Repo(location);
   }
+};
+
+GitFiddle.Gist = function(location){
+  this.id = (location && location.pathname)? location.pathname.match(/^\/([0-9]+)\//)[1] : "";
+
+  this.sounds_like_a_fiddle = function(){
+    var selector = ["#files .file[id^=file_fiddle\\.css]",
+                    "#files .file[id^=file_fiddle\\.html]",
+                    "#files .file[id^=file_fiddle\\.js]",
+                    "#files .file[id^=file_fiddle\\.manifest]"].join(",");
+    return document.querySelector(selector);
+  };
+
+  this.insert_link = function(){
+    var fiddle_link = new GitFiddle.LinksGist(this.id).build();
+    document.querySelector('#repos .meta table tbody').appendChild(fiddle_link);
+  };
+};
+
+GitFiddle.Repo = function(){};
+
+GitFiddle.LinksGist = function(gist){
+  this.url = (function(gist_id){
+    var fiddle_base = 'http://jsfiddle.net/gh/gist/mootools/1.2/';
+    return fiddle_base + gist_id + '/';
+  })(gist.id);
+
+  this.link = (function(url){
+    var link = document.createElement('a');
+    link.className = 'gist-fiddle-link';
+    link.href = url;
+    link.textContent = url;
+    return link;
+  })(this.url);
+
+  this.label = (function(){
+    var label = document.createElement("td");
+    label.textContent = 'Run Jasmine Specs';
+    label.className = 'label';
+    return label;
+  })();
+
+  this.build = function() {
+    var tr = document.createElement('tr');
+    tr.appendChild(this.label);
+    var td = document.createElement('td');
+    td.appendChild(this.link);
+    tr.appendChild(td);
+    return tr;
+  };
 };
