cb4ae1a15c5598f8d1f5c61e1459fd1d4600cc25
Jason Karns <jason@karns.name>
2012-07-02 00:12:26 -0400

    refactor specs to use affix_file() helper and sounds_like_a_fiddle() helper

--- gitfiddle/gitfiddle.spec.js
+++ gitfiddle/gitfiddle.spec.js
@@ -1,4 +1,6 @@
 describe('GitFiddle',function(){
+  var subject;
+
   context('when location is a gist', function(){
     var location = {host: "gist.github.com"};
 
@@ -46,39 +48,41 @@ describe('GitFiddle',function(){
 
 describe('Gist', function(){
   describe('#sounds_like_a_fiddle', function(){
-    var file;
-    beforeEach(function(){
-      file = affix('#files .file').find('.file');
-    });
+    var affix_file = function(id){
+      affix('#files .file').find('.file').attr('id', id);
+    };
+    var sounds_like_a_fiddle = function(){
+      return new GitFiddle.Gist().sounds_like_a_fiddle();
+    };
     describe('is true if', function(){
       it('fiddle.css is found', function(){
-        file.attr('id', 'file_fiddle.css');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
+        affix_file('file_fiddle.css');
+        expect(sounds_like_a_fiddle()).toBeTruthy();
       });
       it('fiddle.html is found', function(){
-        file.attr('id', 'file_fiddle.html');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
+        affix_file('file_fiddle.html');
+        expect(sounds_like_a_fiddle()).toBeTruthy();
       });
       it('fiddle.js is found', function(){
-        file.attr('id', 'file_fiddle.js');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
+        affix_file('file_fiddle.js');
+        expect(sounds_like_a_fiddle()).toBeTruthy();
       });
       it('fiddle.manifest is found', function(){
-        file.attr('id', 'file_fiddle.manifest');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeTruthy();
+        affix_file('file_fiddle.manifest');
+        expect(sounds_like_a_fiddle()).toBeTruthy();
       });
     });
     describe('is false if', function(){
       it('fiddle.(html|css|js|manifest) is not found', function(){
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeFalsy();
+        expect(sounds_like_a_fiddle()).toBeFalsy();
       });
       it('if fiddle.fake is found', function(){
-        file.attr('id', 'file_fiddle.fake');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeFalsy();
+        affix_file('file_fiddle.fake');
+        expect(sounds_like_a_fiddle()).toBeFalsy();
       });
       it('if fiddle is found', function(){
-        file.attr('id', 'file_fiddle');
-        expect(new GitFiddle.Gist().sounds_like_a_fiddle()).toBeFalsy();
+        affix_file('file_fiddle');
+        expect(sounds_like_a_fiddle()).toBeFalsy();
       });
     });
   });
