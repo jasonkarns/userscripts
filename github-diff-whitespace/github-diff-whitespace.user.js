b6b85ed3d25c9cbd6afd84af6eae00f29205c60f
Jason Karns <jason@karns.name>
2012-07-07 15:21:20 -0400

    add <code> element to fix height problem

--- github-diff-whitespace/github-diff-whitespace.user.js
+++ github-diff-whitespace/github-diff-whitespace.user.js
@@ -37,11 +37,14 @@
   };
 
   var addWhitespaceToggleButton = function( button_group, querystring ){
+    var code = document.createElement( 'code' ); // this is a hack to fix some weird height issues with their CSS
+
     var a = document.createElement( 'a' )
     a.className = button_group.querySelector( 'a' ).className;
     a.href = '?' + querystring;
     a.title = 'Toggle Whitespace';
     a.textContent = ' \u2423 ';
+    a.appendChild(code);
 
     var li = document.createElement( 'li' )
     li.appendChild( a );
