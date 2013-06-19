349ce6a52a37565a7fbf87e8466be55f41455652
Jason Karns <jason@karns.name>
2013-06-18 23:14:32 -0400

    Update to latest GitHub release (and simplify)

--- gitrainbow/gitrainbow.js
+++ gitrainbow/gitrainbow.js
@@ -2,17 +2,5 @@ Array.prototype.forEach.call(document.querySelectorAll('.commit-group-item'), fu
   var sha = commit.querySelector('.sha');
   var color = sha.textContent.match(/.{6}/)[0];
 
-  sha.parentElement.style.position = 'relative';
-
-  var mask = document.createElement('span');
-    mask.style.backgroundColor = "#" + color;
-    mask.style.position = "absolute";
-    mask.style.top = 0;
-    mask.style.left = 0;
-    mask.style.width = "100%";
-    mask.style.height = "100%";
-    mask.style.borderRadius = 2;
-    mask.style.opacity = 0.4;
-
-  sha.parentElement.appendChild(mask);
+  sha.parentElement.style.backgroundImage = "linear-gradient(#F2F5F6, #"+ color +")";
 });
